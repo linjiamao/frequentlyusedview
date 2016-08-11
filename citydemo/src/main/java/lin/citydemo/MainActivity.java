538441fba41c75c257822fc41f63cca2cde8e825
ilgnep <ilgnep@gmail.com>
2016-08-11 16:22:04 +0800

    添加城市导航

---
+++ citydemo/src/main/java/lin/citydemo/MainActivity.java
@@ -0,0 +1,16 @@
+package lin.citydemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 城市导航(按字母导航)
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
